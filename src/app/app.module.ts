7586c947ad0694deda6f96b889271ce65d033513
littlehorseboy <littlehorseboy@gmail.com>
2020-04-07 15:50:24 +0800

    feat: ng g module article -m app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleModule } from './article/article.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ArticleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
